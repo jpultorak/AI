88f33debc12ec972d727c71aca17f67fda377edf
jpultorak <jasiustasiu333@gmail.com>
2023-03-24 17:46:56 +0100

    added caching to nonogram solver

--- prac2/nonograms.py
+++ prac2/nonograms.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 
+
 class Nonogram:
 
     def __init__(self, no_rows, no_cols, rows, cols) -> None:
@@ -8,14 +9,17 @@ class Nonogram:
         self.no_cols = no_cols
         self.rows = rows
         self.cols = cols
+        # caching results from opt_dist
+        self.cache = dict()
 
         # board initialization
         self.board =  None
         self.bad_rows = []
         self.reset()
         self.get_bad_rows()
+
         # maximum iterations when solving puzzle
-        self.MAX_ITERATIONS = 500
+        self.MAX_ITERATIONS = (self.no_rows + self.no_cols)*100
 
     def __repr__(self) -> str:
         res = ''
@@ -27,7 +31,50 @@ class Nonogram:
                     res += '.'
             res += '\n'
         return res[:-1]
+    
+    def opt_dist(self, row, row_desc):
+        row_tpl = tuple(row)
+        #print(row, row_desc)
+        if (row_tpl, row_desc) in self.cache:
+            return self.cache[(row_tpl, row_desc)]
+        
+        n = len(row)
+        k = len(row_desc)
+        ones = [0 for _ in range(n+1)]
+        for i in range(1, n+1):
+            ones[i] = ones[i-1]
+            if row[i-1] == 1:
+                ones[i] += 1
 
+        dp = [[1000 for _ in range(n+1)] for _ in range(k+2)]
+        for j in range(1, n+1):
+            dp[k+1][j] = ones[n] - ones[j-1]
+
+        for i in range(k, 0, -1):
+            block_len = row_desc[i-1]
+            for j in range(1, n+1):
+                for l in range(j, n-block_len+2):
+                    r = l+block_len-1
+        
+                    n1 = block_len - (ones[r] - ones[l-1])
+                    n2 = 0
+                    if l != j:
+                        n2 = ones[l-1] - ones[j-1]
+                
+                    n3 = 0 
+                    if r+1 != n+1 and row[r] == 1:
+                        n3 = 1
+
+                    n4 = 1000
+                    if i+1 == k+1 and r+2 > n:
+                        n4 = 0
+                    elif r+2 <= n:    
+                        n4 = dp[i+1][r+2]
+
+                    dp[i][j] = min(dp[i][j], n4 + n1 + n2 + n3)
+        self.cache[(row_tpl, row_desc)] = dp[1][1]    
+        return dp[1][1]
+    
     def flip(self, px):
         return 1-px
     
@@ -35,7 +82,7 @@ class Nonogram:
         if self.bad_rows:
             return False
         for x, col in enumerate(self.board.T):
-            if opt_dist(col, self.cols[x]) > 0:
+            if self.opt_dist(col, self.cols[x]) > 0:
                 return False
         return True
     
@@ -43,7 +90,7 @@ class Nonogram:
         bad_rows = []
         
         for x, row in enumerate(self.board):
-            if opt_dist(row, self.rows[x]) > 0:
+            if self.opt_dist(row, self.rows[x]) > 0:
                 bad_rows.append(x)
         
         self.bad_rows = bad_rows
@@ -54,26 +101,26 @@ class Nonogram:
 
     # how much flipping pixel improves board state
     def change_row(self, row_id, px):
-        block_length = self.rows[row_id]
-        prev_val = opt_dist(self.board[row_id], block_length)
+        row_desc = self.rows[row_id]
+        prev_val = self.opt_dist(self.board[row_id], row_desc)
         
         # flip pixel
         row_1 = np.copy(self.board[row_id])
-        row_1[row_id] = self.flip(row_1[row_id])
-        after_val = opt_dist(row_1, block_length) 
+        row_1[px] = self.flip(row_1[px])
+        after_val = self.opt_dist(row_1, row_desc) 
 
         return prev_val- after_val
     
     def change_col(self, col_id, px):
-        block_length = self.cols[col_id]
+        col_desc = self.cols[col_id]
 
         col = self.board[:, col_id]
-        prev_val = opt_dist(col, block_length)
+        prev_val = self.opt_dist(col, col_desc)
 
         # flip pixel
         col_1 = np.copy(col)
         col_1[px] = self.flip(col_1[px])
-        after_val = opt_dist(col_1, block_length) 
+        after_val = self.opt_dist(col_1, col_desc) 
 
         return prev_val- after_val
     
@@ -88,13 +135,13 @@ class Nonogram:
                 return
             
             # if there are no bad rows 
-            if not self.bad_rows or self.do_random_stfuff(0.1):
+            if not self.bad_rows or self.do_random_stfuff(0.2):
                 row_id = np.random.randint(0, self.no_rows)
             else:
                 row_id = random.choice(self.bad_rows)
 
             # at random choose random column
-            if self.do_random_stfuff(0.05):
+            if self.do_random_stfuff(0.2):
                 col_id = np.random.randint(0, self.no_cols)
             else:
                 # otherwise choosing best cell in that row
@@ -110,17 +157,17 @@ def read_row(s):
     s = s.strip()
     arr = s.split()
     arr = [int(x) for x in arr]
-    return(arr)
+    return tuple(arr)
+
 if __name__ == '__main__':
     with open('zad_input.txt', 'r') as inp, open('zad_output.txt', 'w') as out:
-        s = inp.readline()
-        no_rows, no_cols = int(s[0]), int(s[2:])
+        s = inp.readline().split()
+    
+        no_rows, no_cols = int(s[0]), int(s[1])
         rows = [read_row(inp.readline()) for _ in range(no_rows)]
         cols = [read_row(inp.readline()) for _ in range(no_cols)]
-        print(no_rows, no_cols)
-        print(rows)
-        print(cols)
-    
-        #print(test)
+        game = Nonogram(no_rows, no_cols, rows, cols)
+  
+        game.solve()
+        out.write(str(game))
        
-        #print(test)
